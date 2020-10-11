a2effec71b02ef597b50c3b4392cf08a60550069
IselinLulu <65400419+IselinLulu@users.noreply.github.com>
2020-10-11 15:18:06 +0200

    iselin-halvorsen-pf-ma2

---
+++ script.js
@@ -0,0 +1,68 @@
+//queston 1
+
+var petArray = ["cat","cow","dog"];
+
+
+//question 2
+
+var petArray = ["cat","cow","dog"];
+
+console.log(petArray[1]);
+
+//question 3
+
+var petArray = ["cat","cow","dog"];
+
+petArray.push("sheep")
+
+//question 4
+
+for (var i=0; i < petArray.length; i++){ 
+console.log(petArray.length);
+}
+
+//question 5
+
+var catObject ={
+  name: "Milo",
+  colour: "grey",
+  age: 5
+};
+
+//question 6
+
+console.log(catObject.age);
+
+
+//question 7
+
+var catArray = [
+{
+  name: "Milo",
+  colour: "grey",
+  age: 5
+}
+];
+
+
+//question 8
+
+for(var i=0; i < catArray.length; i++){
+console.log(catArray[i].name)
+console.log(catArray[i].colour)
+console.log(catArray[i].age)
+}
+
+
+//question 9
+
+function logToConsole(oneWord) {
+console.log(oneWord)
+}
+
+//question 10
+
+logToConsole("donkey");
+
+
+
